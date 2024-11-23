d036ce6da94037958fc1c00a3fe7b1ccbd24e08a
Cam Curry <camdcurry@gmail.com>
2024-11-23 13:52:56 -0600

    Initial Commit

---
+++ DrawingClassifier/DrawingClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingClassifierApp.swift
+//  DrawingClassifier
+//
+//  Created by Cam Curry on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct DrawingClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
